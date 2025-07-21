24bf785eee02ab3042816f48f0f8ca8f0b3f9225
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-21 15:09:38 -0400

    updated test_views.py to test for topic list view

--- mtg_blog/tests/test_views.py
+++ mtg_blog/tests/test_views.py
@@ -17,3 +17,18 @@ def test_home_view_renders_topic(client):
     response = client.get(reverse('home'))
     assert response.status_code == 200
     assert b'Red Aggro' in response.content
+
+def test_topic_list_view(self):
+
+    Topic.objects.create(name='Favourite Art', slug='favourite-art')
+    Topic.objects.create(name='MTG Lore', slug='mtg-lore')
+
+    response = self.client.get('/topics/')
+
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'Favourite Art')
+    self.assertContains(response, 'MTG Lore')
+
+    topics = response.context['topic_list']
+    self.assertEqual(topics[0].name, 'Favourite Art')
+    self.assertEqual(topics[1].name, 'MTG Lore')
