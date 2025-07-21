01163f2a699c2a5566c4ceae35b99bb1d9c59156
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-21 18:02:30 -0400

    added path to mtg_blog/urls.py

--- mtg_blog/urls.py
+++ mtg_blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('topics/', views.TopicListView.as_view(), name='topic_list'),
 ]
