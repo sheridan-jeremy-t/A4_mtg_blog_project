24041c6e250cf9f71c25fdaae34032e6c3f754a2
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-21 15:17:59 -0400

    added TopicListView class to views.py

--- mtg_blog/views.py
+++ mtg_blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Count
+from django.views.generic import ListView, DetailView
 from .models import Topic
 
 def home(request):
@@ -10,3 +11,13 @@ def home(request):
         .order_by('-num_posts')[:10]
     )
     return render(request, 'mtg_blog_app/home.html', {'topics': topics})
+
+class TopicListView(ListView):
+    '''List all topics alphabetically'''
+
+    model = Topic
+    template_name = 'mtg_blog.templates/topic_list.html'
+    context_object_name = 'topic_list'
+
+    def get_query(selfself):
+        return Topic.objects.all().order_by('name')
