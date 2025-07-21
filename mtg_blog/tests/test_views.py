0b130d1e57d8a27e74939657f0d85f3bf82ac885
sheridan-jeremy-t <tempestj@sheridancollege.ca>
2025-07-21 19:05:50 -0400

    added tests to test_views.py for Topic Detail View

--- mtg_blog/tests/test_views.py
+++ mtg_blog/tests/test_views.py
@@ -18,7 +18,7 @@ def test_home_view_renders_topic(client):
     assert response.status_code == 200
     assert b'Red Aggro' in response.content
 @pytest.mark.django_db
-def test_topic_list_view(client, db):
+def test_topic_list_view(client):
 
     Topic.objects.create(name='Favourite Art', slug='favourite-art')
     Topic.objects.create(name='MTG Lore', slug='mtg-lore')
@@ -29,5 +29,32 @@ def test_topic_list_view(client, db):
     assert 'MTG Lore' in response.content.decode()
 
     topics = response.context['topic_list']
-    assert topics[0].name, 'Favourite Art'
-    assert topics[1].name, 'MTG Lore'
+    assert topics[0].name == 'Favourite Art'
+    assert topics[1].name == 'MTG Lore'
+
+@pytest.mark.django_db
+def test_topic_detail_view(client):
+    user = User.objects.create_user(username='testuser', password='pass')
+    topic = Topic.objects.create(name='Planeswalker', slug='planeswalker')
+
+    published_post = Post.objects.create(
+        title = 'Published Post',
+        content = 'Published Content',
+        author = user,
+        status = 'published',
+    )
+    published_post.topics.add(topic)
+
+    draft_post = Post.objects.create(
+        title = 'Draft Post',
+        content = 'Draft Content',
+        author = user,
+        status = 'draft'
+    )
+    draft_post.topics.add(topic)
+
+    response = client.get(reverse('mtg_blog_app:topic_detail', kwargs={'slug': topic.slug}))
+    assert response.status_code == 200
+    assert 'Published Post' in response.content.decode()
+    assert 'Draft Post' not in response.content.decode()
+    assert response.context['topic'] == topic
